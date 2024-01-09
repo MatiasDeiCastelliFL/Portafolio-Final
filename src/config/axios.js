6d46c6546c8e328d332d868dc7dfc3173172feac
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2024-01-09 13:50:32 -0300

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const urlAPi = axios.create({
-  baseURL: "https://portafolio-brq7.onrender.com",
+  baseURL: "https://matias-marcelo-dei-castelli-psi.vercel.app/",
 });
